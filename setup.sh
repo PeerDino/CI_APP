f2e5c054c8490d4b1ee89335cb2512d3205a2ed5
Peer Dino <136565121+PeerDino@users.noreply.github.com>
2023-07-22 11:02:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"20cs064@students.muet.edu.pk\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
